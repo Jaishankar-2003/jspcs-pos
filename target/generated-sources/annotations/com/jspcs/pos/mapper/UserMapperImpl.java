d487a85b7fbe250b37aad12c1a9adf4bd4ea49e3
jaishankar-2003 <jaishankar98421@gmail.com>
2026-01-17 09:11:03 +0530

    test

--- target/generated-sources/annotations/com/jspcs/pos/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/jspcs/pos/mapper/UserMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-17T09:04:32+0530",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260101-2150, environment: Java 21.0.2 (Eclipse Adoptium)"
+    date = "2026-01-17T09:08:00+0530",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260101-2150, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
